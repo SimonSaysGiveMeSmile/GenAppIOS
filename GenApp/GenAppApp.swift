97f08724548167e46b7763fc639918155c7a8095
SimonSaysGiveMeSmile <tianjiahe11@gmail.com>
2025-11-17 19:55:32 -0800

    Initial Commit

---
+++ GenApp/GenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenAppApp.swift
+//  GenApp
+//
+//  Created by Simy's MacBook Pro on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GenAppTests/GenAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  GenAppTests.swift
+//  GenAppTests
+//
+//  Created by Simy's MacBook Pro on 11/17/25.
+//
+
+import Testing
+
+struct GenAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
